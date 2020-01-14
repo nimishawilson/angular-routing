aced01cb5ab2bce8765ea469b53f0fd8f9196972
Nimisha Wilson <nimishawilson123@gmail.com>
2020-01-14 22:10:08 +0530

    passing static data to route

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -9,6 +9,7 @@ import { PageNotFoundComponent } from "./page-not-found/page-not-found.component
 import { Routes, RouterModule } from "@angular/router";
 import { AuthGuard } from "./auth-guard.service";
 import { CanDeactivateGuard } from "./servers/edit-server/can-deactivate-guard.service";
+import { ErrorPageComponent } from "./error-page/error-page.component";
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent },
@@ -23,7 +24,8 @@ const appRoutes: Routes = [
           { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard] },
     ]
      },
-    { path: 'not-found', component: PageNotFoundComponent },
+    // { path: 'not-found', component: PageNotFoundComponent },
+     { path: 'not-found', component: ErrorPageComponent, data: { message: 'page not found' } },
     { path: '**', redirectTo: '/not-found', pathMatch: 'full' },
     
   ];
