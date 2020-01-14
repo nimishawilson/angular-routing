66d826bcd1b0cd86297fbbaf42478f5c19f1b9f1
Nimisha Wilson <nimishawilson123@gmail.com>
2020-01-14 11:01:36 +0530

    wildcard routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,11 +11,24 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'users', component: UsersComponent },
-  { path: 'servers', component: ServersComponent },
+  { path: 'users', component: UsersComponent, 
+  children: [
+  { path: ':id/:name', component: UserComponent },
+  ] 
+  },
+  { path: 'servers', component: ServersComponent, 
+  children: [
+         { path: ':id', component: ServerComponent },
+        { path: ':id/edit', component: EditServerComponent },
+  ]
+   },
+  { path: 'not-found', component: PageNotFoundComponent },
+  { path: '**', redirectTo: '/not-found', pathMatch: 'full' },
+  
 ];
 
 @NgModule({
@@ -26,7 +39,8 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
